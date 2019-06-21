58859c6fc24844ba6c80eaf77f8972275a806f1b
Emma Sharon <emmasharon19@gmail.com>
2019-06-21 14:12:55 +0300

    add files to the structure

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+#Initializing applicaton
+app = Flask(__name__)
+
+def create_app(config_name):
+    #....
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # setting config
+    from .requests import configure_request
+    configure_request(app)
+
+    return app
+
+from .main import views
