acb7f8b8bec13c4d310e7b966b1912e00386dc4f
Emma Sharon <emmasharon19@gmail.com>
2019-06-21 12:06:18 +0300

    define objects for the Source class

---
+++ app/models.py
@@ -0,0 +1,18 @@
+class Source:
+    '''
+    Source class to define Source Objects
+    '''
+
+    def __init__(self,name,author,title,description,url,publishedAt,content):
+        self.name =name
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.publishedAt = publishedAt
+        self.content = content
+        
+class Articles:
+    all_articles = []
+
+    def __init__(self,)
