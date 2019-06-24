df3428ed4f3f4a49deedaa438d43527180265998
Emma Sharon <emmasharon19@gmail.com>
2019-06-24 14:35:48 +0300

    edit errors

--- app/main/views.py
+++ app/main/views.py
@@ -1,27 +1,24 @@
 from flask import render_template,request,redirect,url_for
-from app import app
-from ..requests import get_sources
-from ..models import Sources
+from . import main
+from ..requests import get_sources,get_top_headlines,get_everything
+# from ..models import Sources
 
 # Views
-@app.route('/')
+@main.route('/')
 def index():
 
    '''
    View root page function that returns the index page and its data
    '''
-   category_general = get_sources('general')
-   category_business = get_sources('business')
-   category_entertainment = get_sources('entertainment')
-   category_sports = get_sources('sports')
-   category_technology = get_sources('technology')
-   category_science = get_sources('science')
-   title = 'Welcome to the best News service'
-   return render_template('index.html', title=title, general=category_general, business=category_business, entertainment=category_entertainment, sports=category_sports, technology=category_technology, science=category_science)
+   sources = get_sources()
+   Everything = get_everything()
+   print(sources)
+   title = 'Top Highlights '
+   return render_template('index.html', title = title, sources = sources, Everything = Everything)
 
-@app.route('/articles/<source_title>')
-def articles(source_title):
+@main.route('/sources/<sources>')
+def Top_Headlines(sources):
    '''
-   View movie page function that returns the movie details page and its data
+   View headlines page function that returns the Top-headlines from source
    '''
-   return render_template('articles.html',title = source_title)
+   return render_template('Top_Headlines.html',sources = sources, Top_headlines = Top_headlines)
--- config.py
+++ config.py
@@ -1,10 +1,15 @@
+import os
+
 class Config:
     '''
     General configuration parent class
     '''
-    NEWS_API_KEY = '5012e1c88574424db29c6106b935bc1e'
-    NEWS_API_BASE_URL = 'https://newsapi.org/v2/everything?sources?api_key=5012e1c88574424db29c6106b935bc1e'
-
+    # NEWS_API_KEY = '5012e1c88574424db29c6106b935bc1e'
+    # NEWS_API_BASE_URL = 'https://newsapi.org/v2/everything?sources?api_key=5012e1c88574424db29c6106b935bc1e'
+    NEWS_HIGHLIGHT_API_BASE_URL ='https://newsapi.org/v2/sources?apiKey={}'
+    TOP_HEADLINES_URL ='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    EVERYHTHING_URL ='https://newsapi.org/v2/everything?q=bitcoin&apiKey={}'
+    NEWS_HIGHLIGHT_API_URL= os.environ.get('NEWS_HIGHLIGHT_API_KEY')
 class ProdConfig(Config):
     '''
     Production  configuration child class
@@ -24,3 +29,8 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
+    
+config_options = {
+  'development' : DevConfig,
+  'production' : ProdConfig
+}
